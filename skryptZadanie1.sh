00b80019b9ae6a42e6335e1ea0791aeddd3fce90
cyta <piotrstachnio@gmail.com>
2020-03-17 17:56:21 +0100

    final

--- skryptZadanie1.sh
+++ skryptZadanie1.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+# dir pierwszy argument przyjmowany przy wywolywaniu skryptu
 
-mkdir -p $1/A/B -p $1/A/C/D $1/E
-touch $1/A/plik1.txt $1/A/B/plik2.txt $1/A/C/D/plik3.txt $1/E/plik4.txt
+dir=$1
 
-echo "Test 1" >> $1/A/plik1.txt
-echo "Test 2" >> $1/A/B/plik2.txt
-echo "Test 3" >> $1/A/C/D/plik3.txt
-echo "Test 4" >> $1/E/plik4.txt
 
-ln -s $PWD/$1/A/plik1.txt $PWD/$1/E/link_bez_plik1
-ln -s $PWD/$1/E/plik4.txt $PWD/$1/E/link_bez_plik4
-ln -s ../plik1.txt $1/A/B/link_plik1
-ln -s ../A/B/plik2.txt $1/E/link_plik2
-ln -s ../A/C/D/plik3.txt $1/E/link_plik3
-ln -s ../../../E/plik4.txt $1/A/C/D/link_plik4
+mkdir -p $dir/A/B -p $dir/A/C/D $dir/E
+touch $dir/A/plik1.txt $dir/A/B/plik2.txt $dir/A/C/D/plik3.txt $dir/E/plik4.txt
+
+echo "Test 1" >> $dir/A/plik1.txt
+echo "Test 2" >> $dir/A/B/plik2.txt
+echo "Test 3" >> $dir/A/C/D/plik3.txt
+echo "Test 4" >> $dir/E/plik4.txt
+
+ln -s $PWD/$dir/A/plik1.txt $PWD/$dir/E/link_bez_plik1
+ln -s $PWD/$dir/E/plik4.txt $PWD/$dir/E/link_bez_plik4
+ln -s ../plik1.txt $dir/A/B/link_plik1
+ln -s ../A/B/plik2.txt $dir/E/link_plik2
+ln -s ../A/C/D/plik3.txt $dir/E/link_plik3
+ln -s ../../../E/plik4.txt $dir/A/C/D/link_plik4
--- skryptZadanie2.sh
+++ skryptZadanie2.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-./skryptZadanie1.sh  $1
+dir=$1
 
-cp -P $PWD/$1/E/link_bez_plik1 $PWD/$1/A/C/
-cp -P $PWD/$1/E/link_plik3 $PWD/$1/A/C/
+./skryptZadanie1.sh  $dir
 
-# wzgledny link nie dziala. Powodem jest zmiania sciezki do pliku z ktorym jest on polaczony wzgledem jego pozycji w drzewie folderow
+cp -P $PWD/$dir/E/link_bez_plik1 $PWD/$dir/A/C/
+cp -P $PWD/$dir/E/link_plik3 $PWD/$dir/A/C/
+
+#wzgledny link nie dziala. Powodem jest zmiania sciezki do pliku z ktorym jest on polaczony wzgledem jego pozycji w drzewie folderow
 #bezwgledny link dziala poniewaz adres jego targetu nie jest uzalezniony od jego(linku) polozenia
-cp -L $PWD/$1/E/link_bez_plik4 $PWD/$1/A/C/
+cp -L $PWD/$dir/E/link_bez_plik4 $PWD/$dir/A/C/
 
