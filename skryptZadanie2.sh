f8cc9879b896d43b7f2aae26e06348eca73533bd
cyta <piotrstachnio@gmail.com>
2020-03-17 17:58:02 +0100

    final

--- skryptZadanie2.sh
+++ skryptZadanie2.sh
@@ -6,7 +6,7 @@ dir=$1
 cp -P $PWD/$dir/E/link_bez_plik1 $PWD/$dir/A/C/
 cp -P $PWD/$dir/E/link_plik3 $PWD/$dir/A/C/
 
-#wzgledny link nie dziala. Powodem jest zmiania sciezki do pliku z ktorym jest on polaczony wzgledem jego pozycji w drzewie folderow
+#wzgledny link nie dziala. Poniewaz zmieniajac polozenie linku wzglednego musimy zmieniec rowniez sciezke do targetu 
 #bezwgledny link dziala poniewaz adres jego targetu nie jest uzalezniony od jego(linku) polozenia
 cp -L $PWD/$dir/E/link_bez_plik4 $PWD/$dir/A/C/
 
